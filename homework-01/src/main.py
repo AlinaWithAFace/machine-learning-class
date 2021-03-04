4c6f8a5322373293195319496627dcd4befb0a2b
Alexa Bennett <aprange@udel.edu>
2021-03-04 14:48:30 -0500

    main.py

--- homework-01/src/main.py
+++ homework-01/src/main.py
@@ -16,27 +16,29 @@ to-print:{yes,no}
 
 #print(sys.argv)
 
-L = sys.argv[1]
-K = sys.argv[2]
-training_set_path = sys.argv[3]
-validation_set_path = sys.argv[4]
-test_set_path = sys.argv[5]
-to_print_input = sys.argv[6]
+def initalize():
+    L = sys.argv[1]
+    K = sys.argv[2]
+    training_set_path = sys.argv[3]
+    validation_set_path = sys.argv[4]
+    test_set_path = sys.argv[5]
+    to_print_input = sys.argv[6]
 
-training_set = pandas.read_csv(training_set_path)
-validation_set = pandas.read_csv(validation_set_path)
-test_set = pandas.read_csv(test_set_path)
+    training_set = pandas.read_csv(training_set_path)
+    validation_set = pandas.read_csv(validation_set_path)
+    test_set = pandas.read_csv(test_set_path)
 
-if to_print_input == "yes":
-    to_print = True
-elif to_print_input == "no":
-    to_print = False
-else:
-    print("to-print must be 'yes' or 'no', printing anyway")
-    to_print = True
+    if to_print_input == "yes":
+        to_print = True
+    elif to_print_input == "no":
+        to_print = False
+    else:
+        print("to-print must be 'yes' or 'no', printing anyway")
+        to_print = True
 
 def node_formation(df):
     # recursive function which drills down until a pure node is returned
+    # no promises, but I think this recursive structure should work
 
     count_01 = {}
     for attribute in df.loc[:, df.columns != 'Class']:
@@ -44,20 +46,22 @@ def node_formation(df):
         ones = (df[attribute] == 1).sum()
         count_01[attribute] = (zeros, ones)
 
-    # Send dictionary to for variance comparison
+    # Send dictionary for variance comparison
     # That returns the attribute to form a node on ... the lowest value?
 
-    # If value was greate than 0:
-    #    node_formation(df[df[returned_attribute] == 0])
-    #    node_formation(df[df[returned_attribute] == 1])
+    # If value > 0:
+    #    branch_0 = node_formation(df[df[returned_attribute] == 0])
+    #    branch_1 = node_formation(df[df[returned_attribute] == 1])
+    #    return {node: {0: branch_0, 1: branch_1}}
+        
+    # else: # value was 0, set as pure_node with outcome
+    #    return {pure_node: leaf}
+        # pure_node is an attribute "X_"
+        # leaf is 0 or 1
 
-    # If value was 0, set as pure_node with outcome
 
-    return {pure_node: outcome}
-    # pure_node is an attribute "X_"
-    # outcome is 0 or 1
-
-
+if __name__=="__main__":
+initialize()
 node_formation(training_set)
 
 #print("L: {}".format(L))
