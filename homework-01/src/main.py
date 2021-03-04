9eb49d1952857ce76a8625bd7067ee7cc0bfa25b
Alexa Bennett <aprange@udel.edu>
2021-03-04 16:19:07 -0500

    reworked formatting (again, sorry!), tied in calculate_variance. Current output is a nested dictionary for the tree before pruning. I have not verified the output. I would like to have someone double check that the math is as intended.

--- homework-01/src/main.py
+++ homework-01/src/main.py
@@ -16,53 +16,87 @@ to-print:{yes,no}
 
 #print(sys.argv)
 
-def initalize():
-    L = sys.argv[1]
-    K = sys.argv[2]
-    training_set_path = sys.argv[3]
-    validation_set_path = sys.argv[4]
-    test_set_path = sys.argv[5]
-    to_print_input = sys.argv[6]
+L = sys.argv[1]
+K = sys.argv[2]
+training_set_path = sys.argv[3]
+validation_set_path = sys.argv[4]
+test_set_path = sys.argv[5]
+to_print_input = sys.argv[6]
 
-    training_set = pandas.read_csv(training_set_path)
-    validation_set = pandas.read_csv(validation_set_path)
-    test_set = pandas.read_csv(test_set_path)
+training_set = pandas.read_csv(training_set_path)
+validation_set = pandas.read_csv(validation_set_path)
+test_set = pandas.read_csv(test_set_path)
 
-    if to_print_input == "yes":
-        to_print = True
-    elif to_print_input == "no":
-        to_print = False
-    else:
-        print("to-print must be 'yes' or 'no', printing anyway")
-        to_print = True
+if to_print_input == "yes":
+    to_print = True
+elif to_print_input == "no":
+    to_print = False
+else:
+    print("to-print must be 'yes' or 'no', printing anyway")
+    to_print = True
+
 
 def node_formation(df):
     # recursive function which drills down until a pure node is returned
     # no promises, but I think this recursive structure should work
 
     count_01 = {}
-    for attribute in df.loc[:, df.columns != 'Class']:
-        zeros = (df[attribute] == 0).sum()
+    # dictionary for attributes with counts as tuples (0, 1, sum)
+    for attribute in df.loc[:, df.columns != ('Class' or '' or 'None')]:
+        # loops over all attributes not identified as Class, the outcome
+        zeros = (df[attribute] == 0).sum() # Turns df in True/False
         ones = (df[attribute] == 1).sum()
         count_01[attribute] = (zeros, ones)
 
-    # Send dictionary for variance comparison
-    # That returns the attribute to form a node on ... the lowest value?
+    min_variance = calculate_variance(count_01)
+    # returns tuple (attribute, variance)
 
-    # If value > 0:
-    #    branch_0 = node_formation(df[df[returned_attribute] == 0])
-    #    branch_1 = node_formation(df[df[returned_attribute] == 1])
-    #    return {node: {0: branch_0, 1: branch_1}}
+    if min_variance[1] > 0:
+        branch_0 = node_formation(df[df[min_variance[0]] == 0].drop([min_variance[0]], axis=1))
+        branch_1 = node_formation(df[df[min_variance[0]] == 1].drop([min_variance[0]], axis=1))
+        return {min_variance[0]: {0: branch_0, 1: branch_1}}
         
-    # else: # value was 0, set as pure_node with outcome
-    #    return {pure_node: leaf}
+    else: # value was 0, set as pure_node with outcome
+        return {min_variance[0]: min_variance[1]}
         # pure_node is an attribute "X_"
         # leaf is 0 or 1
 
 
-if __name__=="__main__":
-initialize()
-node_formation(training_set)
+def calculate_variance(target_values):
+    #values = list(target_values)
+    #elements,counts = #here we need a function that assigns to "element" the lists of our elements X_i
+                      #and to "counts" the pair [number of 0s in X_i, number of 1s in X_i]
+    #variance_impurity = 0
+    #sum_counts = _sum(counts)
+    #for i in elements:
+     #   variance_impurity += (-counts[i]/sum_counts*(counts[i]/sum_counts))
+
+    # I attempted to rework the formula with the data that comes from node_formation
+    # Please double check my math - Alexa
+
+    attribute_counter = 0
+    min_impurity = ('NotMe!', 1)
+    # all calculations should be less than 1
+    for attribute in target_values:
+        attribute_counter += 1
+        counts = target_values[attribute]
+        # (0s, 1s)
+        zeros = counts[0]
+        ones = counts[1]
+        sum_counts = zeros + ones
+        variance_impurity = (zeros/sum_counts*ones/sum_counts)
+        if min_impurity[1] > variance_impurity:
+            min_impurity = (attribute, variance_impurity)
+
+    if min_impurity[0] == 'NotMe!':
+        print('Ya done messed up.')
+
+    return min_impurity
+
+
+node_dict = node_formation(training_set)
+print(node_dict)
+# TODO: Test that this output aligns with data input
 
 #print("L: {}".format(L))
 #print("K: {}".format(K))
